20d0096cf267238b131d6c555aefa369bfd5a755
Максим <www.maxim2003@ukr.net>
2022-12-14 21:21:02 +0200

    Initial commit #2 added tests

--- src/main/java/com/example/vacancy/services/MainService.java
+++ src/main/java/com/example/vacancy/services/MainService.java
@@ -3,8 +3,12 @@ package com.example.vacancy.services;
 import com.example.vacancy.dto.PersonDTO;
 import com.example.vacancy.repositories.PersonRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.HttpStatusCodeException;
+
+import java.util.Optional;
 
 @Service
 public class MainService {
@@ -12,7 +16,6 @@ public class MainService {
     private PersonRepo personRepo;
     public PersonDTO getPersonFromDb(long id) throws HttpStatusCodeException {
         var person = personRepo.findById(id);
-
         if (person != null) {
             return new PersonDTO(
                     person.getFirstName(),
--- src/test/java/com/example/vacancy/VacancyApplicationTests.java
+++ src/test/java/com/example/vacancy/VacancyApplicationTests.java
@@ -1,13 +1,73 @@
 package com.example.vacancy;
 
+import com.example.vacancy.controllers.MainController;
+import com.example.vacancy.repositories.PersonRepo;
+import com.example.vacancy.services.MainService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.HttpServerErrorException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class VacancyApplicationTests {
 
+    private final static String URL = "jdbc:h2:mem:test";
+    private final static String USER = "sa";
+    private final static String PASSWORD = "";
+    @Autowired
+    private MainController mainController;
+
+    @Autowired
+    private MainService mainService;
+
+    @Autowired
+    private PersonRepo repo;
+
+
     @Test
-    void contextLoads() {
+    public void Test_db_connection() throws Exception {
+        Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        assertThat(connection).isNotNull();
+    }
+    @Test
+    public void Verify_controller_is_not_null() throws Exception {
+        assertThat(mainController).isNotNull();
     }
 
+    @Test
+    public void Verify_service_is_not_null() throws Exception {
+        assertThat(mainService).isNotNull();
+    }
+
+    @Test
+    public void Verify_repo_is_not_null() throws Exception {
+        assertThat(repo).isNotNull();
+    }
+
+    @Test
+    public void Verify_request_invalid_argument_throws_exception() throws Exception {
+        HttpServerErrorException e1 = assertThrows(HttpServerErrorException.class, () -> {
+            mainController.getPerson(0);
+        });
+        HttpServerErrorException e2 = assertThrows(HttpServerErrorException.class, () -> {
+            mainController.getPerson('f');
+        });
+        HttpServerErrorException e3 = assertThrows(HttpServerErrorException.class, () -> {
+            mainController.getPerson(-1);
+        });
+    }
+
+    @Test
+    public void Verify_db_entity_not_found_exception() throws Exception {
+        var lastId = repo.findTopByOrderByIdDesc().getId();
+        HttpServerErrorException e = assertThrows(HttpServerErrorException.class, () -> {
+            mainController.getPerson(lastId+1);
+        });
+    }
 }
